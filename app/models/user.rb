57e8bc5e777a1a2d9f7bbaeed330f647002b7bcd
Tom <tomecrepont@gmail.com>
2021-04-13 15:41:48 +0200

    Creation of 100 fake users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :events
+  has_one_attached :photo
   validates :username, uniqueness: true
   validates :email, uniqueness: true
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-
+require "open-uri"
 # creation of 100 user with a name and a photo
 
 # first unsplash collection of portrait : https://unsplash.com/collections/4281671/minimal-portrait
@@ -17,5 +17,23 @@ require 'faker'
 100.times do |i|
   collection_id = i < 50 ? "4281671" : "1346789"
   unsplash_url = "https://source.unsplash.com/collection/#{collection_id}/1600x900"
+  photo = URI.open(unsplash_url)
+  random_first_name = Faker::Name.first_name 
+  random_last_name = Faker::Name.male_first_name
+  username = random_first_name.downcase+random_last_name.downcase
+  email = Faker::Internet.safe_email(name: username)
+  password = "test1234"
+  puts "Creation of a new User Instance for #{random_first_name} #{random_last_name}"
+  user = User.create({
+    last_name: random_last_name,
+    first_name: random_first_name,
+    password: password,
+    email: email,
+    username: username
+  })
+  puts "Adding the random portrait to the user..."
+  user.photo.attach(io: photo, filename: "#{username}.png", content_type: 'image/png')
+  puts "done."
+  puts "New User Instance for #{random_first_name} #{random_last_name} succesfully created"
 
 end
