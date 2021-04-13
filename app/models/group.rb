10fe5ef2fec44c74121818f58b10407d7bed8b45
Tom <tomecrepont@gmail.com>
2021-04-13 18:08:15 +0200

    Sofistication of the models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :events
+  has_many :group_members
 end
--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,4 +1,5 @@
 class GroupMember < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  has_many :event_invitations
 end
