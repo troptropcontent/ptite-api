858cfc7d4010a975789b4caa58214f933154166f
Tom <tomecrepont@gmail.com>
2021-04-13 15:08:00 +0200

    Added first and last name to User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# creation of 100 user with a name and a photo
+
+# first unsplash collection of portrait : https://unsplash.com/collections/4281671/minimal-portrait
+# first unsplash collection of portrait : https://unsplash.com/collections/1346789/portrait-reference
+# unsplash url to request https://source.unsplash.com/collection/${collection_id}/1600x900
+
+100.times do |i|
+  collection_id = i < 50 ? "4281671" : "1346789"
+  unsplash_url = "https://source.unsplash.com/collection/#{collection_id}/1600x900"
+
+end
