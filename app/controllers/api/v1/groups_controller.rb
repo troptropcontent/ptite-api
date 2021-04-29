6ec770df4c96d62f1af1fbc240282cc818878fa0
Tom Ecrepont <tomecrepont@gmail.com>
2021-04-29 18:48:41 +0200

    Creation of the create method to receive the post request from the front app

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::GroupsController < ApplicationController
   skip_before_action :require_login, only: [:create]
   def create
-    raise
+    new_group = current_user.groups.create({name: group_params[:name] })
+    users_rehydrated = group_params[:users].map{|user_id| User.find(user_id)}
+    users_rehydrated.each{|user| new_group.group_members.create({user: user})}
+    
+    render json: new_group.serialized
 
   end
 
@@ -16,7 +20,7 @@ class Api::V1::GroupsController < ApplicationController
   private 
 
   def group_params
-    params.permit(:name, :users)
+    params.permit(:name,:users => [])
   end
 
 end
